786a439a6833cb6f92394cbf32cf3424285835b2
Erik Natanael Gustafsson <erik@eriknatanael.com>
2024-09-19 16:25:32 +0200

    add start script

---
+++ kiosk_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk file:///home/loam/Documents/loam_pi/visualize/index.html &
